8400c457886bde8b13f229b7db2a5ff0b201dc0d
Dries Bos <info@driesbos.com>
2019-09-05 10:25:13 +0200

    added shoppinglist + likes views

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -9,77 +9,97 @@ export const clearInput = () => {
 };
 
 export const clearResults = () => {
-  elements.searchResList.innerHTML = ''
-  elements.searchResPages.innerHTML = ''
-}
+  elements.searchResList.innerHTML = '';
+  elements.searchResPages.innerHTML = '';
+};
 
 export const highlightSelected = id => {
-  const resultsArr = Array.from(document.querySelectorAll('.results__link'))
+  const resultsArr = Array.from(document.querySelectorAll('.results__link'));
   resultsArr.forEach(el => {
-    el.classList.remove('results__link--active')
-  })
-  documents.queryselector(`a[href="#${id}"]`).classList.add('results__link--active')
-}
+    el.classList.remove('results__link--active');
+  });
+  document.querySelector(`.results__link[href*="${id}"]`).classList.add('results__link--active');
+};
 
+/*
+// 'Pasta with tomato and spinach'
+acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
+acc: 5 / acc + cur.length = 9 / newTitle = ['Pasta', 'with']
+acc: 9 / acc + cur.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 18 / acc + cur.length = 24 / newTitle = ['Pasta', 'with', 'tomato']
+*/
 export const limitRecipeTitle = (title, limit = 17) => {
-  const newTitle = []
+  const newTitle = [];
   if (title.length > limit) {
     title.split(' ').reduce((acc, cur) => {
       if (acc + cur.length <= limit) {
-        newTitle.push(cur)
+        newTitle.push(cur);
       }
-      return acc + cur.length
-    }, 0)
-    return `${newTitle.join(' ')} ...`
+      return acc + cur.length;
+    }, 0);
+
+    // return the result
+    return `${newTitle.join(' ')} ...`;
   }
-  return title
+  return title;
 }
+
 const renderRecipe = recipe => {
   const markup = `
-      <li>
-          <a class="results__link" href="#${recipe.recipe_id}">
-              <figure class="results__fig">
-                  <img src="${recipe.image_url}" alt="${recipe.title}">
-              </figure>
-              <div class="results__data">
-                  <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
-                  <p class="results__author">${recipe.publisher}</p>
-              </div>
-          </a>
-      </li>
-  `;
+        <li>
+            <a class="results__link" href="#${recipe.recipe_id}">
+                <figure class="results__fig">
+                    <img src="${recipe.image_url}" alt="${recipe.title}">
+                </figure>
+                <div class="results__data">
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+                    <p class="results__author">${recipe.publisher}</p>
+                </div>
+            </a>
+        </li>
+    `;
   elements.searchResList.insertAdjacentHTML('beforeend', markup);
 };
 
+// type: 'prev' or 'next'
 const createButton = (page, type) => `
-  <button class="btn-inline results__btn--${type}" data-goto="${type === 'prev' ? page - 1 : page + 1}">
-      <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
-      <svg class="search__icon">
-          <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
-      </svg>
-  </button>
-`
+    <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+    </button>
+`;
 
 const renderButtons = (page, numResults, resPerPage) => {
-  const pages = Math.ceil(numResults / resPerPage)
-  let button
+  const pages = Math.ceil(numResults / resPerPage);
+
+  let button;
   if (page === 1 && pages > 1) {
-    button = createButton(page, 'next')
+    // Only button to go to next page
+    button = createButton(page, 'next');
   } else if (page < pages) {
-    button =
-      `
-    ${button = createButton(page, 'prev')}
-    ${button = createButton(page, 'next')}
-    `
+    // Both buttons
+    button = `
+            ${createButton(page, 'prev')}
+            ${createButton(page, 'next')}
+        `;
   } else if (page === pages && pages > 1) {
-    button = createButton(page, 'prev')
+    // Only button to go to prev page
+    button = createButton(page, 'prev');
   }
-  elements.searchResPages.insertAdjacentHTML('afterbegin', button)
-}
+
+  elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+};
 
 export const renderResults = (recipes, page = 1, resPerPage = 10) => {
-  const start = (page - 1) * resPerPage
-  const end = (page * resPerPage)
+  // render results of currente page
+  const start = (page - 1) * resPerPage;
+  const end = page * resPerPage;
+
   recipes.slice(start, end).forEach(renderRecipe);
-  renderButtons(page, recipes.length, resPerPage)
+
+  // render pagination buttons
+  renderButtons(page, recipes.length, resPerPage);
 };
